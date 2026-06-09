722204d954d7049bbd25f28c97a6727b3b387801
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-06-09 14:47:08 -0300

    exericio Fire 11

--- ex11.js
+++ ex11.js
@@ -1,10 +1,3 @@
-// Atividade 11 - Dobro dos números
-// Crie um array com 5 números.
-// Crie um segundo array vazio chamado dobro.
-// Utilize um for.
-// Armazene no segundo vetor o dobro de cada valor do primeiro.
-// Mostre os dois vetores.
-
 let numeros = [10, 5, 6, 7, 42]
 let dobro = []
 
