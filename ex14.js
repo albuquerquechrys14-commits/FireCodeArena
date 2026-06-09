0a6d76398edf53076e5ad818f6f6d9a2ce198df5
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-06-09 14:43:39 -0300

    feat: subindo exericio 14

---
+++ ex14.js
@@ -0,0 +1,50 @@
+// Atividade 14 - Estatísticas da turma
+// Peça ao usuário quantas notas deseja cadastrar.
+// Armazene todas em um vetor.
+// Ao final, mostre:
+// Maior nota
+// Menor nota
+// Média da turma
+// Quantidade de aprovados (nota ≥ 7)
+// Quantidade de reprovados (nota < 7)
+
+const readline = require ('readline-sync')
+console.log("=============== TURMA ================")
+let notas = []
+let quantidade = readline.questionInt (`Quantas notas deseja cadastrar? `)
+
+for (let i = 0; i < quantidade; i++){
+    const nota = readline.questionFloat (`Digite a nota ${i + 1}: `)
+    notas.push(nota)
+}
+
+let maiorNota = notas[0]
+let menorNota = notas[0]
+let soma = 0
+let aprovados = 0
+let reprovados = 0
+
+for (let i = 0; i < notas.length; i++){
+    if (notas[i] > maiorNota) {
+        maiorNota = notas[i]
+    }
+    if (notas[i] < menorNota) {
+        menorNota = notas[i]
+    }
+    soma += notas[i]
+
+    if (notas[i] >= 7) {
+        aprovados++
+    } else {
+        reprovados++
+    }
+}
+
+const media = soma / notas.length
+console.log("")
+console.log("=============== Resultados ===============")
+console.log(`Maior nota: ${maiorNota}`)
+console.log(`Menor nota: ${menorNota}`)
+console.log(`Média da turma: ${media.toFixed(2)}`)
+console.log(`Quantidade de aprovados: ${aprovados}`)
+console.log(`Quantidade de reprovados: ${reprovados}`)
