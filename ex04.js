2e7bb6e18ce2e6c0fd072d66d316ce87ce9be839
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-06-09 14:46:13 -0300

    exericio Fire 4

--- ex04.js
+++ ex04.js
@@ -1,9 +1,3 @@
-// Atividade 04 - Encontrando o maior número
-// Crie um array com 5 números.
-// Utilize um for para percorrê-lo.
-// Descubra qual é o maior valor armazenado.
-// Mostre o resultado no console.
-
 let numeros = [10, 5, 8, 3, 12]
 let maiorNumero = numeros[0]
 
