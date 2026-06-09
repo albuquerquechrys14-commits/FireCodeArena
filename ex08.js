4d02cca6d915410ba26c7b7fa5563559e169c011
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-06-09 14:46:42 -0300

    exericio Fire 8

--- ex08.js
+++ ex08.js
@@ -1,10 +1,3 @@
-// Atividade 08 - Quantidade de números positivos
-// Crie um array com 10 números.
-// Utilize um for.
-// Conte quantos números são positivos.
-// Mostre o resultado final.
-
-
 let numeros = [67, -42, 68, 77, -88,90, -102, 66, 50, -98]
 
 let positivos = 0
