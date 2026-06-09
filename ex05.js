cfb9b038340f77cddf7ffcf7b6500f0a84ab3672
albuquerquechrys14-commits <albuquerque.chrys14@gmail.com>
2026-06-09 13:53:29 -0300

    feat: subindo exericio 5

---
+++ ex05.js
@@ -0,0 +1,16 @@
+// Atividade 05 - Encontrando o menor número
+// Crie um array com 5 números.
+// Utilize um for para percorrer o vetor.
+// Descubra qual é o menor valor armazenado.
+// Mostre o resultado no console.
+
+let numeros = [10, 5, 8, 3, 12]
+let menorNumero = numeros[0]
+
+for (let i = 1; i < numeros.length; i++){
+ if (numeros[i] < menorNumero){
+    menorNumero = numeros[i]
+
+ }
+}
+console.log(`resultado: ${menorNumero}`)
